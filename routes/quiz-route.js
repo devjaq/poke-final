7618c02c2b85ea078d6f28139d0f4c0491bfe28b
hannahsaintonge <hannahsaintonge@gmail.com>
2018-06-19 11:02:59 -0400

    started the quiz component

--- routes/quiz-route.js
+++ routes/quiz-route.js
@@ -1,2 +1,19 @@
 "use strict";
 
+const express = require("express");
+const http = require("http");
+const request = require("request");
+
+const pg = require("pg");
+const pool = require("../pg-connection-pool");
+
+const quizRouter = express.Router();
+
+quizRouter.get("/quiz", (req, res) => {
+    pool.query("SELECT * FROM questions ORDER BY id").then((result) => {
+        res.send(result.rows);
+        console.log(req.body);
+    });
+});
+
+module.exports = quizRouter;
