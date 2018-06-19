a6a72480a07a1f90d3d815296b514205c1a6a844
Nick Burnell <nicholas.burnell92@gmail.com>
2018-06-19 13:50:23 -0400

    working on quiz component

--- app/public/scripts/components/quiz.js
+++ app/public/scripts/components/quiz.js
@@ -3,25 +3,36 @@
 const quiz = {
   template: `
   <h1>Welcome Come take our Quiz to see what pokeman you get!!!</h1>
-  <section ng-repeat="question in $ctrl.quizarr | orderBy : 'id'">
-      <h3>{{ question.question }}</h3>
-      <ul>
-        <li>{{question.answer_1}}</li>
-        <li>{{question.answer_2}}</li>
-        <li>{{question.answer_3}}</li>
-        <li>{{question.answer_4}}</li>
-      </ul>
+  <form ng-submit="$ctrl.addUser($ctrl.newUser)">
+    <h3>Enter a Username..</h3>
+    <input type="text" ng-model="$ctrl.newUser.username">
+    <h3>Enter your Date of Birth</h3>
+    <input type="date" ng-model="$ctrl.newUser.dob">
+    <section ng-repeat="question in $ctrl.quizarr | orderBy : 'id'">
+      <form>
+        <h3>{{ question.question }}</h3>
+        <input type="radio" ng-model="$ctrl.newUser" name="{{question.id}}" value="{{question.answer_1_value}}"> {{question.answer_1}}
+        <input type="radio" ng-model="" name="{{question.id}}" value="{{question.answer_2_value}}"> {{question.answer_2}}
+        <input type="radio" ng-model="" name="{{question.id}}" value="{{question.answer_3_value}}"> {{question.answer_3}}
+        <input type="radio" ng-model="" name="{{question.id}}" value="{{question.answer_4_value}}"> {{question.answer_4}}
+      </form>
     </section>
+    <button>Submit</button>
+  </form>
   `,
 
-  controller: ["quizService", function(quizService) {
+  controller: ["quizService", function (quizService) {
     const vm = this;
     vm.quizarr = [];
 
     quizService.getQuiz().then((response) => {
-      console.log("should work");  
+      console.log("should work");
       vm.quizarr = response.data;
     });
+
+    vm.addUser = (newUser) => {
+      console.log(newUser);
+    }
   }]
 
 }
