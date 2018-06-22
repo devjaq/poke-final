b1161b0e2179bff2c63d3a068e55a397b25c64e3
Nick Burnell <nicholas.burnell92@gmail.com>
2018-06-22 13:27:07 -0400

    added pokemon text box

--- app/public/scripts/components/start.js
+++ app/public/scripts/components/start.js
@@ -4,7 +4,7 @@ const start = {
 template: `
 <section class="start">
   <div>
-    <p>Hello there! Welcome to the world of Pokémon! My name is Grant Chirpus! People call me the Pokémon Prof! Your very own Pokémon adventure is about to unfold! A world of dreams and adventures with Pokémon awaits! Let’s go!</p>
+    <p class="pokeBox">Hello there! Welcome to the world of Pokémon! My name is Grant Chirpus! People call me the Pokémon Prof! Your very own Pokémon adventure is about to unfold! A world of dreams and adventures with Pokémon awaits! Let’s go!</p>
     <img src="../../styles/lab-ready-grant.png">
   </div>
   <button type="button" ng-click="$ctrl.start();">Get my First Pokemon!</button>
