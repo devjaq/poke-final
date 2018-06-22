52605db71447606a5688d8f0c34da597c30221bb
Nick Burnell <nicholas.burnell92@gmail.com>
2018-06-22 14:22:24 -0400

    working on start page css

--- app/public/scripts/components/start.js
+++ app/public/scripts/components/start.js
@@ -2,12 +2,14 @@
 
 const start = {
 template: `
-<section class="start">
-  <div>
-    <p class="pokeBox">Hello there! Welcome to the world of Pokémon! My name is Grant Chirpus! People call me the Pokémon Prof! Your very own Pokémon adventure is about to unfold! A world of dreams and adventures with Pokémon awaits! Let’s go!</p>
-    <img src="../../styles/lab-ready-grant.png">
+<section class="main">
+  <img src="../../styles/lab-ready-grant.png" class="grant">
+  <div class="start">
+    <div class="textContainer">
+      <p class="pokeBox">Hello there! Welcome to the world of Pokémon! My name is Grant Chirpus! People call me the Pokémon Prof! Your very own Pokémon adventure is about to unfold! A world of dreams and adventures with Pokémon awaits! Let’s go!</p>
+    </div>
+    <button type="button" ng-click="$ctrl.start();">Get my First Pokemon!</button>
   </div>
-  <button type="button" ng-click="$ctrl.start();">Get my First Pokemon!</button>
 </section>  
 `,
 
