b95432fc089212b508d932ea297557c6cb2d1d7c
Nick Burnell <nicholas.burnell92@gmail.com>
2018-06-25 19:12:12 -0400

    added mobile swipe directive, working confirmed

--- app/public/scripts/components/start.js
+++ app/public/scripts/components/start.js
@@ -2,7 +2,7 @@
 
 const start = {
 template: `
-<section class="main">
+<section id="slideButton" class="main slide" ng-swipe-up="$ctrl.swipeUp()" ng-swipe-down="$ctrl.swipeDown()">
 <div class="bg-gradient">
   <img src="../../styles/full-logo.png" class="start-logo">
   <img src="../../styles/lab-ready-grant.png" class="grant">
@@ -27,6 +27,30 @@ controller: ["$location", function($location) {
 
     vm.battle = () => {
       $location.path("/battle")
+    }
+    vm.swipeUp = function () {
+      var arrow = $("#slideButton");
+      console.log("swipe-up");
+      if ($("#slideButton").hasClass("slide") == true) {
+        console.log("true");
+        // arrow.css({
+        //   "transform": "rotate(180deg)",
+        //   "transition-duration": "2200ms"
+        // });
+        $location.path("/quiz");
+      }
+    }
+    vm.swipeDown = function () {
+      var arrow = $("#slideButton");
+      console.log("swipe-down");
+      if (arrow.hasClass("slide") == false) {
+        console.log("false");
+        // arrow.css({
+        //   "transform": "rotate(0deg)",
+        //   "transition-duration": "2200ms"
+        // });
+        $location.path("/quiz");
+      }
     }
 
 }]
--- app/public/scripts/module.js
+++ app/public/scripts/module.js
@@ -1,4 +1,4 @@
 "use strict";
 
 angular 
-  .module("App", ['ngRoute'])
+  .module("App", ['ngRoute', "swipe", "ngTouch"])
